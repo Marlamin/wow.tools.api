97637919334c5a0d90446941910c1b5d6c8667de
Martin Benjamins <marlamin@marlamin.com>
2020-01-30 00:38:57 +0100

    Document TACTKey properties

--- Models/TACTKey.cs
+++ Models/TACTKey.cs
@@ -7,9 +7,24 @@ namespace wow.tools.api.Models
 {
     public class TACTKey
     {
+        /// <summary>
+        /// ID from TactKey.db2.
+        /// </summary>
         public int ID { get; set; }
+
+        /// <summary>
+        /// Hex representation of 8-byte lookup from TactKeyLookup.db2.
+        /// </summary>
         public string Lookup { get; set; }
+
+        /// <summary>
+        /// Hex representation of 16-byte key from TactKey.db2 and/or hotfixes.
+        /// </summary>
         public string Key { get; set; }
+
+        /// <summary>
+        /// Manually set description of what this key encrypts.
+        /// </summary>
         public string Description { get; set; }
     }
 }
